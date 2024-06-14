07f8aaf4ff3a6938aa6a00b55e00b14337548d1d
B@C <aungthuyeinhein98@gmail.com>
2024-06-14 18:03:37 +0630

    create header section

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { Button } from "flowbite-react";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex flex-wrap gap-2">
+      <Button gradientDuoTone="purpleToBlue">Purple to Blue</Button>
+      <Button gradientDuoTone="cyanToBlue">Cyan to Blue</Button>
+      <Button gradientDuoTone="greenToBlue">Green to Blue</Button>
+      <Button gradientDuoTone="purpleToPink">Purple to Pink</Button>
+      <Button gradientDuoTone="pinkToOrange">Pink to Orange</Button>
+      <Button gradientDuoTone="tealToLime">Teal to Lime</Button>
+      <Button gradientDuoTone="redToYellow">Red to Yellow</Button>
+    </div>
+  );
 }
