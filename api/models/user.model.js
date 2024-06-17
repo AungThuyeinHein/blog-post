35d38141daadf0be26bab91d1aa224edc814da32
B@C <aungthuyeinhein98@gmail.com>
2024-06-17 20:39:39 +0630

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema();
+
+const userschema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userschema);
+
+export default User;
