15abd4241dc3ff2fb5f2ada3c8d5f6c0eb514e33
B@C <aungthuyeinhein98@gmail.com>
2024-07-07 01:46:24 +0630

    create signin api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { errorHandler } from "../utlis/error.js";
+import jwt from "jsonwebtoken";
+
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -23,4 +25,33 @@ export const signUp = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required!!!"));
+  }
+  try {
+    const validUser = await User.findOne({ email }).select("+password");
+    if (!validUser) {
+      return next(
+        errorHandler(400, "Invalid User Credentials. Please Check again...")
+      );
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(
+        errorHandler(400, "Invalid User Credentials. Please Check again...")
+      );
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRECT);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {}
 };
