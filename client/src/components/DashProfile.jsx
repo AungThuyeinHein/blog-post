6ce1f0dd33ea0ed78c7dfbf4317dcb3d30ff7e2e
B@C <aungthuyeinhein98@gmail.com>
2024-07-11 04:57:06 +0630

    destructuring currentuser

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -2,7 +2,7 @@ import { Button, TextInput } from "flowbite-react";
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
-  const currentUser = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
